3fb249345a7c99bb08d5e34b50d84a014084cd10
nicholasefe <nicholasefe4000@yahoo.com>
2024-04-15 07:44:35 +0100

    Updated

--- website/hos.py
+++ website/hos.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import pathlib
+import sqlite3
+
+base_path = pathlib.Path(r'C:\Users\nicho\Github\project_group_13\database')
+db_name = "hospital.db"
+db_path = base_path / db_name
+print(db_path)
 
 hos = Flask(__name__)
 
@@ -12,8 +19,18 @@ def about():
 
 @hos.route("/data")
 def data():
-    students = ["Efe", "Okoh", "0857500"]
-    return render_template("data.html", students=students)
+    con = sqlite3.connect(db_path)
+    cursor = con.cursor()
+    lst = cursor.execute('SELECT * FROM hospital_table Limit 30').fetchall()
+    con.close()
+    
+    # for title
+    column_headers = ['Provider_ID', 'Hospital_Name', 'Address', 'City', 'State', 'ZIP_Code',
+       'County_Name', 'Hospital_Type', 'Hospital_Ownership',
+       'Emergency_Services', 'Disease_Name', 'Days', 'Predicted_Cases',
+       'Observed_Cases', 'SIR']
+    
+    return render_template("data.html", column=column_headers, hospital_features=lst)
 
 if __name__=="__main__":
     hos.run(debug=True)
